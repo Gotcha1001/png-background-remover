453f7024ce1cfbf12c27334c0f4ba15636907fb2
WesleyWayneOlivier <WesleyOlivier443@gmail.com>
2025-06-13 20:15:49 +0200

    Pie

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    esmExternals: "loose", // Allows Next.js to handle ESM dependencies flexibly
+  },
+};
 
 export default nextConfig;
